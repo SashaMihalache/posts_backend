cb50d62d8361b072b751351b50919d217687ec0f
sasha mihalache <sasha.mihalache@gmail.com>
2022-11-03 20:04:57 +0200

    add mongoose and networking from express server

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose
+  .connect("mongodb://admin:testtest@mongo:27017/?authSource=admin")
+  .then(() => {
+    console.log("Successfully connected to the Database.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 app.get("/", (req, res) => {
-  res.send("<h2>Hello World !!!</h2>");
+  res.send("<h2>Hello World</h2>");
 });
 
 const port = process.env.PORT || 3000;
